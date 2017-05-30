6224a8c320d3a9b58553288e589be6e5a87fe678
Nate <nr265@cornell.edu>
2017-05-30 20:06:34 +0000

    Priner seems to work now, more details to come

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from flask import render_template, request, flash, redirect, url_for
 from app import app
+from Adafruit_Thermal import *
+import textwrap
+
+printer = Adafruit_Thermal("/dev/ttyUSB0", 19200, timeout = 5)
 
 @app.route("/")
 
@@ -13,7 +17,8 @@ def printlist():
   f=request.form
   for k in f.keys():
     for v in f.getlist(k):
-      print(v)
+      if v: print(v)
+      if v: printer.println(textwrap.fill(v,32))
   print("testing print list")
   flash('List created')
   return redirect(url_for("list"))
