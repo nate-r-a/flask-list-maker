8da5ce1dc08ee069d9b10ce690891859c44d4d45
Nate <nr265@cornell.edu>
2017-05-31 00:18:34 +0000

    More updates

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from flask import render_template, request, flash, redirect, url_for
 from app import app
 from Adafruit_Thermal import *
 import textwrap
 
 printer = Adafruit_Thermal("/dev/ttyUSB0", 19200, timeout = 5)
+printer.boldOn()
+printer.setSize('L')
 
 @app.route("/")
 
@@ -17,8 +21,17 @@ def printlist():
   f=request.form
   for k in f.keys():
     for v in f.getlist(k):
-      if v: print(v)
-      if v: printer.println(textwrap.fill(v,32))
+      if v:
+        print(v)
+        lines = textwrap.wrap(("- "+v),32)
+        #for line in lines:
+          #line = line + " ║".decode("utf-8")
+          #line.decode("utf-8")
+        #lines = list(map((lambda x: x+ u" ║"), lines)
+        for line in lines:
+          printer.println(line)
+    #print [v.encode('ascii') for v in f.getlist(k)]
+    if f.getlist(k) != ['']: printer.feed(3)
   print("testing print list")
   flash('List created')
   return redirect(url_for("list"))
